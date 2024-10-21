b4fb37b51b0c79aad794298754781877a4438048
gonzaloriveraf <gonzalo.rivera.91@gmail.com>
2024-10-21 13:16:06 -0300

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: '@strapi/provider-upload-cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+          secure: true 
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          transformation: {
+            quality: 'auto:best',
+            fetch_format: 'auto',
+          },
+          delete: {},
+        },
+      },
+    },
+  });
